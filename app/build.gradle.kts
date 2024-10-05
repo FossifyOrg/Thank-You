1e9d7972894a1e08a2cbdcbe96123395ac571bc3
Naveen Singh <snaveen935@gmail.com>
2024-10-05 20:01:56 +0530

    Exclude dependency info from APKs
    Because this info is encrypted and only readable by Google. It serves no purpose in foss releases.
    
    See https://github.com/FossifyOrg/Phone/issues/43 for more info.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,6 +91,10 @@ android {
         targetCompatibility = currentJavaVersionFromLibs
     }
 
+    dependenciesInfo {
+        includeInApk = false
+    }
+
     tasks.withType<KotlinCompile> {
         kotlinOptions.jvmTarget = project.libs.versions.app.build.kotlinJVMTarget.get()
     }
