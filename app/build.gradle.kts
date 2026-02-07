f006a692bb7305fdf00e3c4c00836816e7f1242b
Naveen Singh <snaveen935@gmail.com>
2026-02-07 21:55:36 +0530

    build: use directories set instead of srcDirs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -104,7 +104,7 @@ android {
     }
 
     sourceSets {
-        getByName("main").java.srcDirs("src/main/kotlin")
+        getByName("main").java.directories.add("src/main/kotlin")
     }
 
     compileOptions {
